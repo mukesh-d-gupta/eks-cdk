0c3ea2107a40e5ef7827e4d37317876f2949890b
EC2 Default User <ec2-user@ip-10-0-5-231.ap-southeast-2.compute.internal>
2023-03-05 09:55:44 +0000

    Initial commit

---
+++ test/eks.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Eks from '../lib/eks-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Eks.EksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
